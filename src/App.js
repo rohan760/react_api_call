f98c4cea4ad2b2629cea13deb6b4beda11b403b9
rohan760 <coderrohan1234@gmail.com>
2023-01-05 22:06:07 +0530

    api_call_useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Stack, TextField } from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
+
+const baseURL = "https://jsonplaceholder.typicode.com/users";
 
 function App() {
+  const [users, setUsers] = useState("not found");
+  const [value, setValue] = useState("");
+
+  const handleClick = async (_e) => {
+    const res = await axios.get(baseURL);
+    let userData = res.data;
+
+    let usernames = [];
+    for (let i in userData) usernames.push(userData[i].username);
+    setUsers(usernames.filter((user) => user.includes(value)));
+    console.log(users);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stack spacing={2} direction="row">
+        <TextField
+          label="type username"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button variant="contained" color="secondary" onClick={handleClick}>
+          get username
+        </Button>
+        <div>{users}</div>
+      </Stack>
     </div>
   );
 }
